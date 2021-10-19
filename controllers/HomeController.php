00ea60d38bb727aaa5193bc9e4acc8bdee1cc17b
wido <widoroveta@gmail.com>
2021-10-18 21:16:19 -0300

    again

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -5,7 +5,13 @@
         public function index($message=""){
             require_once(VIEWS_PATH.'home.php');
         }
-    
+        
+        public function showHomeLogin($message=""){
+            require_once(VIEWS_PATH."home-login.php");
+        }
+        public function register(){
+          
+        }
         public function logout(){
       
             $_SESSION['loggedUser']= null;
