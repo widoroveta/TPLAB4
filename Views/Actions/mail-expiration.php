993984a43d780487921babf1889aef4ad315eb35
Guido Roveta <55321418+widoroveta@users.noreply.github.com>
2023-07-30 19:38:38 -0300

    Update mail-expiration.php

--- Views/Actions/mail-expiration.php
+++ Views/Actions/mail-expiration.php
@@ -9,7 +9,7 @@ $mail->isSMTP();
 $mail->Host = 'smtp.office365.com';
 $mail->SMTPAuth = true;
 $mail->Username = 'guido.roveta@alumnos.mdp.utn.edu.ar';
-$mail->Password = 'Altoguiso147';
+//$mail->Password = 'Altoguiso147';
 $mail->SMTPSecure = 'tls';
 $mail->Port = 587;
 $mail->setFrom('guido.roveta@alumnos.mdp.utn.edu.ar', 'Utn reg mardelplata');
